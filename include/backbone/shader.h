e7a5f14739af045a31bb9e2ffa75990ba557a691
osaka <osaka@cool.com>
2025-05-21 23:47:01 -0500

    Revert "ok please work im sorry"
    This reverts commit 4d738a9109073f796c80f3499b198e14108515b3.

--- include/backbone/shader.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHADER_H_INCLUDE
-#define SHADER_H_INCLUDE
-
-unsigned int loadShader(const char *path, unsigned int program);
-
-void deleteShader(unsigned int shader);
-
-
-#endif // end SHADER_H_INCLUDE
