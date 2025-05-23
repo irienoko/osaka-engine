3fb745785842314c74e13e113dcbecbca52d2d2c
osaka <osaka@cool.com>
2025-05-23 15:25:42 -0500

    adding back player camera we can move around now :D

--- source/main.c
+++ source/main.c
@@ -13,6 +13,22 @@
 #include "thirdparty/vec.h"
 
 void error_callback(int error, const char* description);
+void processInput(GLFWwindow *window);
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+vec3 camera_pos = {0.0f, 0.0f,  3.0f};
+vec3 camera_front = {0.0f, 0.0f, -1.0f};
+vec3 camera_up = {0.0f, 1.0f, 0.0f};
+
+float deltatime = 0.0f;
+float lasttime = 0.0f;
+
+float yaw   = -90.0f;
+float pitch =  0.0f;
+
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
 
 int main(int argc, char **argv)
 {
@@ -36,6 +52,7 @@ int main(int argc, char **argv)
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetCursorPosCallback(window, mouse_callback);
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         printf("Failed to initialize OpenGL context\n");
@@ -58,8 +75,6 @@ int main(int argc, char **argv)
     deleteShader(vertexShader);
     deleteShader(fragmentShader);
 
-    //end of load basic shader
-
 
     Model orange = loadObjFromFile("bin/models/teto_orange.obj");
     Mesh_index orange_mesh = indexVBO(orange);
@@ -72,6 +87,12 @@ int main(int argc, char **argv)
         glfwGetFramebufferSize(window, &width, &height);
         glViewport(0, 0, width, height);
 
+        float currentTime = (float)glfwGetTime();
+        deltatime = currentTime - lasttime;
+        lasttime = currentTime;
+
+        processInput(window);
+
         glClearColor(0.2f, 0.3f,0.3f,1.0f);
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -80,7 +101,9 @@ int main(int argc, char **argv)
 
         mat4x4 view;
         mat4x4_identity(view);
-        mat4x4_translate(view, 0.0f, 0.0f, -4.0f);
+        vec3 reference = {0};
+        vec3_add(reference, camera_pos, camera_front);
+        mat4x4_look_at(view, camera_pos, reference, camera_up);
 
         mat4x4 projection;
         mat4x4_identity(projection);
@@ -114,6 +137,85 @@ int main(int argc, char **argv)
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    float camreaSpeed = 2.5*deltatime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        vec3 camera_move = {0};
+        vec3_scale(camera_move, camera_front, camreaSpeed);
+        camera_pos[0] += camera_move[0];
+        camera_pos[1] += camera_move[1];
+        camera_pos[2] += camera_move[2];
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        vec3 camera_move = {0};
+        vec3_scale(camera_move, camera_front, camreaSpeed);
+        camera_pos[0] -= camera_move[0];
+        camera_pos[1] -= camera_move[1];
+        camera_pos[2] -= camera_move[2];
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        vec3 camera_cross = {0};
+        vec3_mul_cross(camera_cross, camera_front, camera_up);
+        vec3_norm(camera_cross, camera_cross);
+
+        vec3 camera_move = {0};
+        vec3_scale(camera_move, camera_cross, camreaSpeed);
+
+        camera_pos[0] += camera_move[0];
+        camera_pos[1] += camera_move[1];
+        camera_pos[2] += camera_move[2];
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        vec3 camera_cross = {0};
+        vec3_mul_cross(camera_cross, camera_front, camera_up);
+        vec3_norm(camera_cross, camera_cross);
+
+        vec3 camera_move = {0};
+        vec3_scale(camera_move, camera_cross, camreaSpeed);
+
+        camera_pos[0] -= camera_move[0];
+        camera_pos[1] += camera_move[1];
+        camera_pos[2] -= camera_move[2];
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.1f * deltatime;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+
+    vec3 direction = {0};
+    direction[0] = cos(yaw) * cos(pitch);
+    direction[1] = sin(pitch);
+    direction[2] = sin(yaw) * cos(pitch);
+    vec3_norm(direction, direction);
+    vec3_dup(camera_front, direction);
 }
 
 void error_callback(int error, const char* description)
